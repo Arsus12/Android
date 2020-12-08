7339898873f9803a27e1bb684391cb6c06090d81
Arsus <warcrafttral@mail.ru>
2020-12-08 17:53:02 +0700

    Создал Cat и вывел результат в TextView

--- Lab42/app/src/main/java/com/example/lab4/MainActivity.java
+++ Lab42/app/src/main/java/com/example/lab4/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.lab4;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Cat();
+    }
+    public class Cat {
+
+        public String name; // имя
+        public int age; // возраст
+        public int color; // цвет
+
+    }
+    private void Cat(){
+        Cat murzik = new Cat();
+        murzik.name = "Мурзик";
+        murzik.age = 9;
+        murzik.color = Color.BLACK;
+
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(gson.toJson(murzik));
     }
 }
+
