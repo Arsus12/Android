3b4bc542d8fe1a3f122bcd33c0f1b488e6a5699e
Arsus <warcrafttral@mail.ru>
2020-12-17 16:13:57 +0700

    Создал окно Touch

--- Lab52/app/src/main/java/com/example/lab5/Event_Handling.java
+++ Lab52/app/src/main/java/com/example/lab5/Event_Handling.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 public class Event_Handling extends AppCompatActivity implements View.OnClickListener {
 
     Button btnPressMe;
+    Button Touch;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,12 +17,20 @@ public class Event_Handling extends AppCompatActivity implements View.OnClickLis
 
         btnPressMe = (Button) findViewById(R.id.task1);
         btnPressMe.setOnClickListener(this);
+
+        Touch = (Button) findViewById(R.id.task2);
+        Touch.setOnClickListener(this);
     }
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.task1:
                 Intent intent = new Intent(this, Task1.class);
                 startActivity(intent);
+                break;
+
+            case R.id.task2:
+                Intent intent1 = new Intent(this, Touch.class);
+                startActivity(intent1);
                 break;
             default:
                 break;
