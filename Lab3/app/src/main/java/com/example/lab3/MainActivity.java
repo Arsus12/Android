f63d2095b3d1f425bbb2fad586099a939aa04814
Arsus <warcrafttral@mail.ru>
2020-12-22 19:22:39 +0700

    Вставил RecyclerView, добавил код в MainActivity, задал textSize

---
+++ Lab3/app/src/main/java/com/example/lab3/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.lab3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+    public static class DummyContent {
+        /** Списочный массив элементов */
+        public static final List<DummyItem> ITEMS = new ArrayList<DummyItem>();
+        private static final int COUNT = 25;
+
+        static {
+            // Добавление элементов в список.
+            for (int k = 1; k <= COUNT; k++) {
+                StringBuilder builder = new StringBuilder();
+                builder.append("Информация об элементе:   ").append(k);
+                for (int j = 0; j < k; j++) { builder.append("\n Детальная информация. "); }
+                ITEMS.add(new DummyItem(String.valueOf(k), "Элемент " + k, builder.toString()));
+            }
+        }
+
+        /** Создание класса элементов для списка. */
+        public static class DummyItem {
+            public final String id;
+            public final String content;
+            public final String details;
+
+            public DummyItem(String id, String content, String details) {
+                this.id = id;
+                this.content = content;
+                this.details = details;
+            }
+            @Override
+            public String toString() { return content; }
+        }
+    }
+
+
+}
