30523337aedc06e194734915177af36fcf864d34
Arsus <warcrafttral@mail.ru>
2020-12-22 20:12:01 +0700

    Создал Адаптер

--- Lab3/app/src/main/java/com/example/lab3/MainActivity.java
+++ Lab3/app/src/main/java/com/example/lab3/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.lab3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,36 +17,5 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-    public static class DummyContent {
-        /** Списочный массив элементов */
-        public static final List<DummyItem> ITEMS = new ArrayList<DummyItem>();
-        private static final int COUNT = 25;
-
-        static {
-            // Добавление элементов в список.
-            for (int k = 1; k <= COUNT; k++) {
-                StringBuilder builder = new StringBuilder();
-                builder.append("Информация об элементе:   ").append(k);
-                for (int j = 0; j < k; j++) { builder.append("\n Детальная информация. "); }
-                ITEMS.add(new DummyItem(String.valueOf(k), "Элемент " + k, builder.toString()));
-            }
-        }
-
-        /** Создание класса элементов для списка. */
-        public static class DummyItem {
-            public final String id;
-            public final String content;
-            public final String details;
-
-            public DummyItem(String id, String content, String details) {
-                this.id = id;
-                this.content = content;
-                this.details = details;
-            }
-            @Override
-            public String toString() { return content; }
-        }
-    }
-
 
 }
